0658bfae72e4d3a0e61d21fa5ca498287673f2bf
m4113w <76271787+m4113w@users.noreply.github.com>
2025-05-26 22:16:34 -0400

    Add files via upload

---
+++ excercise1_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/*Now you should not forget your glasses*/
+int main()
+{
+cout << "If this text";
+cout << " appears on your display, ";
+cout << "you can pat yourself on ";
+cout << "the back!" << endl;
+return 0;
+}
